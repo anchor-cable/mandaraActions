0b644ed9686b3bac31f4729a235068a623d20e56
T S <forever.blue.moon@gmail.com>
2019-05-03 16:06:33 +0900

    googleAuth実装

---
+++ mandalaActions/src/components/Editor.vue
@@ -0,0 +1,22 @@
+ <template>
+  <div id="editor">
+    <h1>エディター画面</h1>
+    <h2>{{user.displayName}}</h2>
+    <button @click="googleLogout">Googleアカウントからログアウト</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "editor",
+  props: ["user"],
+  data() {
+    return {};
+  },
+  methods: {
+    googleLogout: function() {
+      firebase.auth().signOut();
+    }
+  }
+};
+</script>
---
+++ mandalaActions/src/components/Home.vue
@@ -0,0 +1,25 @@
+ <template>
+  <div id="home">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <button @click="googleLogin">Googleアカウントでログイン</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      msg: "Welcome to mandalaActions!"
+    };
+  },
+  methods: {
+    googleLogin: function() {
+      firebase
+        .auth()
+        .signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    }
+  }
+};
+</script>
