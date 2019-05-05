c2819396aa592b0b2e28c5255706dec4879e941a
T S <forever.blue.moon@gmail.com>
2019-05-05 18:44:19 +0900

    メモ追加・削除ボタン実装

--- mandalaActions/src/components/Editor.vue
+++ mandalaActions/src/components/Editor.vue
@@ -1,8 +1,27 @@
- <template>
+<template>
   <div id="editor">
     <h1>エディター画面</h1>
-    <h2>{{user.displayName}}</h2>
+    <h2>{{ user.displayName }}</h2>
     <button @click="googleLogout">Googleアカウントからログアウト</button>
+    <div class="editorWrapper">
+      <div class="memoListWrapper">
+        <div
+          class="memoList"
+          v-for="(memo, index) in memos"
+          :key="index"
+          @click="selectMemo(index)"
+          :data-selected="index == selectedIndex"
+        >
+          <p class="memoTitle">{{ displayTitle(memo.inputbox) }}</p>
+        </div>
+        <div>
+          <button class="addMemoBtn" @click="addMemo">メモの追加</button>
+          <button class="deleteMemoBtn" v-if="memos.length >1" @click="deleteMemo">選択中メモの削除</button>
+        </div>
+      </div>
+      <textarea class="inputbox" v-model="memos[selectedIndex].inputbox"></textarea>
+      <div class="preview" v-html="preview()"></div>
+    </div>
   </div>
 </template>
 
@@ -11,12 +30,68 @@ export default {
   name: "editor",
   props: ["user"],
   data() {
-    return {};
+    return {
+      memos: [
+        {
+          inputbox: ""
+        }
+      ],
+      selectedIndex: 0
+    };
   },
   methods: {
     googleLogout: function() {
       firebase.auth().signOut();
+    },
+    addMemo: function() {
+      this.memos.push({
+        inputbox: "無題のメモ"
+      });
+    },
+    deleteMemo: function() {
+      this.memos.splice(this.selectedIndex, 1);
+      if (this.selectedIndex > 0) {
+        this.selectedIndex--;
+      }
+    },
+    selectMemo: function(index) {
+      this.selectedIndex = index;
+    },
+    preview: function() {
+      return this.memos[this.selectedIndex].inputbox;
+    },
+    displayTitle: function(text) {
+      return text.split(/\n/)[0];
     }
   }
 };
 </script>
+<style lang="scss" scoped>
+.editorWrapper {
+  display: flex;
+}
+.memoListWrapper {
+  width: 20%;
+  border-top: 1px solid #000;
+}
+.memoList {
+  padding: 10px;
+  box-sizing: border-box;
+  text-align: left;
+  border-bottom: 1px solid #000;
+  &:ntn-chilid(even) {
+    background-color: #ccc;
+  }
+  &[data-selected="true"] {
+    background-color: #ccf;
+  }
+}
+.inputbox {
+  width: 50%;
+  height: 500px;
+}
+.preview {
+  width: 50%;
+  text-align: left;
+}
+</style>
